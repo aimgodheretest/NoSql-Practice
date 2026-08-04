264959ace1575b67de9ad9bcf937798897ee79f6
Murli Kumar <aimgodhere@gmail.com>
2026-08-04 20:51:01 +0530

    feat: add getCart and removeFromCart methods to user model

--- models/user.js
+++ models/user.js
@@ -56,4 +56,31 @@ userSchema.methods.addToCart = function (product) {
   return this.save();
 };
 
+/*
+Mongoose Instance Method
+*/
+userSchema.methods.getCart = function () {
+  return this.populate("cart.items.productId").then((user) => {
+    return user.cart.items.map((item) => {
+      return {
+        ...item.productId._doc,
+        quantity: item.quantity,
+      };
+    });
+  });
+};
+
+/*
+Mongoose Instance Method
+*/
+userSchema.methods.removeFromCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter((item) => {
+    return item.productId.toString() !== productId.toString();
+  });
+
+  this.cart.items = updatedCartItems;
+
+  return this.save();
+};
+
 module.exports = mongoose.model("User", userSchema);
